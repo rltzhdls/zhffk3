5c212a8de240e9cd1e703b86f479682f510c1bd6
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-27 14:12:06 +0900

    Add files via upload

---
+++ 39cd8c.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/zhzk2@main/coca2.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
